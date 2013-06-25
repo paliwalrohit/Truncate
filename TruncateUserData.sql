6d168ab23dbb3ac27170f9c7447ce29526d6b767
paliwalrohit <rohit.paliwal@vivacityinfotech.com>
2013-06-26 04:24:49 +0530

    truncate lialoka db file

---
+++ TruncateUserData.sql
@@ -0,0 +1,35 @@
+truncate table Address;
+
+truncate table Coupon;
+
+truncate table CouponTrack;
+
+truncate table Favorite;
+
+truncate table FRAsso;
+
+truncate table FRCampaign;
+
+truncate table FRInvite;
+
+truncate table FROrganize;
+
+truncate table FRPayout;
+
+truncate table FRSolicit;
+
+truncate table JoinCoupon;
+
+truncate table Merchant;
+
+truncate table MerchantDescript;
+
+truncate table Subscrip;
+
+truncate table User;
+
+truncate table UserInteract;
+
+truncate table Voucher;
+
+truncate table VoucherTrack;
